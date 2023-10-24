31f39ce3470ed95dec41df2ad2b9601f19b918cc
Alan Huang <148530288+AlanHuangECV@users.noreply.github.com>
2023-10-24 19:08:40 +0800

    Add files via upload

--- test_Neo_API14.py
+++ test_Neo_API14.py
@@ -1183,8 +1183,8 @@ Input_username = ""
 Input_password = ""
 
 # =============設定allure==============================================
-if __name__ == '__main__':
-    pytest.main([r'--alluredir=allure-report'])
+# if __name__ == '__main__':
+pytest.main([r'--alluredir=allure-report'])
     # pytest.main([r'--alluredir=test_report\allure'])
     # pytest.main(["-s", "-v", "--html=Outputs/reports/pytest.html", "--alluredir=Outputs/allure"])   # allure文件生成的目錄
 
@@ -1283,7 +1283,7 @@ assert test_A7test() == None # A7要跟A6一起測
 # assert test_A8test() == "Pass"
 assert test_A11test() == None
 # assert test_A12test() == "Pass"
-os.system("allure generate allure_report/ -o report/html --clean")
+# os.system("allure generate allure_report/ -o report/html --clean")
 #===============================================================
 
 # ===================完成讀取參數===================================================
